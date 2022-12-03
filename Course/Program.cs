c6db33dd5d3dd9ff52c7ef39639864ba2198b129
yang <yang@DESKTOP-BVGGR6R>
2022-12-03 16:34:20 -0300

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            bool completo = false;
+            byte x = 126;
+            int x1 = 1000;
+            int x2 = 221212121;
+            long x3 = 145964644L;
+            char letra = '\u0042';
+            float x4 = 4.5f;
+            double x5 = 4.5;
+            string nome = "yang silva";
+            object obj1 = "alexa";
+            object obj2 = 'T';
+
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            sbyte n4 = sbyte.MaxValue;
+            decimal n5 = decimal.MinusOne;
+            decimal n6 = decimal.MaxValue ;
+            decimal n7 = decimal.MinValue;
+
+
+
+            x++;
+            Console.WriteLine(x);
+            Console.WriteLine(x1);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            Console.WriteLine(x3);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(x4);
+            Console.WriteLine(x5);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(n7);
+
+
+
+
+
+
+
+        }
+    }
+}
